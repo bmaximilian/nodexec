78ceacad1336b99073865c858a928084f3c6a0f5
bmaximilian <maximilianbmail@gmail.com>
2018-05-18 18:11:29 +0200

    Parse options better

--- commands/say:hello.js
+++ commands/say:hello.js
@@ -4,6 +4,10 @@
 */
 
 module.exports = {
-  command: () => console.log('Hello World!'),
+  command: (options) => {
+    console.log(JSON.stringify(options, null, 2));
+    console.log();
+    console.log('Hello World!');
+  },
   scope: '/test',
 }
--- src/util/parseOptions.js
+++ src/util/parseOptions.js
@@ -1,11 +1,13 @@
 
-const { assign, isArray } = require('lodash');
+const { assign, isArray, isString } = require('lodash');
 
 function parseOptions(optionArray) {
-  let buffer = {};
+  let buffer = {
+    params: [],
+  };
   let optionAccumulator = {
     param: null,
-    value: true,
+    value: null,
   };
 
   if (!isArray(optionArray)) {
@@ -15,17 +17,30 @@ function parseOptions(optionArray) {
   optionArray.forEach((option, i) => {
     optionAccumulator[i % 2 === 0 ? 'param' : 'value'] = option;
 
+    if (option.charAt(0) === '-' && (i === optionArray.length - 1 || optionArray[i + 1].charAt(0) === '-')) {
+      optionAccumulator.value = true;
+    } else if (option.charAt(0) !== '-' && (!isString(optionAccumulator.param) || optionAccumulator.param.charAt(0) !== '-')) {
+      optionAccumulator = {
+        param: option,
+        value: option,
+      }
+    }
+
     if (optionAccumulator.value !== null && optionAccumulator.param !== null) {
-      buffer = assign(
-        buffer,
-        {
-          [optionAccumulator.param]: optionAccumulator.value,
-        },
-      );
+      if (optionAccumulator.param === optionAccumulator.value) {
+        buffer.params.push(optionAccumulator.value);
+      } else {
+        buffer = assign(
+          buffer,
+          {
+            [optionAccumulator.param]: optionAccumulator.value,
+          },
+        );
+      }
 
       optionAccumulator = {
         param: null,
-        value: true,
+        value: null,
       };
     }
   });
