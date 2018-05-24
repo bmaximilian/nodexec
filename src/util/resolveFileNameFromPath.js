30fb10d2cabee317485d1a595b00f6bc10670ae6
bmaximilian <maximilianbmail@gmail.com>
2018-05-24 14:38:13 +0200

    0.4.3: Added test for resolveFileNameFromPath

--- src/util/resolveFileNameFromPath.js
+++ src/util/resolveFileNameFromPath.js
@@ -1,3 +1,6 @@
+
+const { isString, isEmpty } = require('lodash');
+
 /**
  * Reads the file name from the submitted path
  *
@@ -5,14 +8,15 @@
  * @returns {String|null} : The filename if the path has a filename otherwise null
  */
 function resolveFileNameFromPath(path) {
-    if (typeof path !== 'string') {
+    if (!isString(path)) {
         throw new Error('The path must be a string.');
     }
 
     const tree = path.split('/');
 
     if (tree && tree.length > 0) {
-        return tree[tree.length - 1];
+        const file = tree[tree.length - 1];
+        return isString(file) && !isEmpty(file) ? file : null;
     }
 
     return null;
---
+++ src/util/test/resolveFileNameFromPath.spec.js
@@ -0,0 +1,41 @@
+/**
+ * Created on 23.05.18.
+ *
+ * @author Maximilian Beck <maximilian.beck@wtl.de>
+ */
+
+const { describe, it } = require('mocha');
+const { expect } = require('chai');
+const resolveFileNameFromPath = require('../resolveFileNameFromPath');
+
+describe('paths', () => {
+    it('Should be a function', () => {
+        expect(resolveFileNameFromPath).to.be.an.instanceOf(Function);
+    });
+
+    it('Should resolve the file name from a path', () => {
+        expect(resolveFileNameFromPath('/my/test/path/file.end')).to.equal('file.end');
+        expect(resolveFileNameFromPath('/my/test/path/file')).to.equal('file');
+        expect(resolveFileNameFromPath('/my/test/path/.env')).to.equal('.env');
+        expect(resolveFileNameFromPath('foo')).to.equal('foo');
+    });
+
+    it('Should return null when the path contains no file', () => {
+        expect(resolveFileNameFromPath('/my/test/path/file/')).to.be.null;
+        expect(resolveFileNameFromPath('/')).to.be.null;
+    });
+
+    it('Should throw when no string is passed', () => {
+        /**
+         * Returns a function that calls resolveFileNameFromPath with the param
+         * @param {*} param : * : The param
+         * @returns {function(): String} : The function
+         */
+        const passAParameter = param => () => resolveFileNameFromPath(param);
+
+        expect(passAParameter(null)).to.throw();
+        expect(passAParameter(1)).to.throw();
+        expect(passAParameter(true)).to.throw();
+        expect(passAParameter('foo')).not.to.throw();
+    });
+});
