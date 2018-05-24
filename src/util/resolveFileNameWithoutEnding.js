6ade33e4df8e5fe7705d86af9f0d6b50866b41e1
bmaximilian <maximilianbmail@gmail.com>
2018-05-24 14:48:56 +0200

    0.4.3: Added test for resolveFileNameWithoutEnding

--- src/util/resolveFileNameWithoutEnding.js
+++ src/util/resolveFileNameWithoutEnding.js
@@ -1,3 +1,5 @@
+const { isString, isEmpty } = require('lodash');
+
 /**
  * Resolves the file name without ending from a file name
  *
@@ -5,15 +7,21 @@
  * @returns {String} : The file name without ending
  */
 function resolveFileNameWithoutEnding(filename) {
-    if (typeof filename !== 'string') {
+    if (!isString(filename)) {
         throw new Error('The file name must be a string.');
+    }
+
+    if (isEmpty(filename) || filename.charAt(filename.length - 1) === '/') {
+        return null;
     }
 
     const buffer = filename.split('.');
 
-    buffer.pop();
+    if (buffer.length > 1 && !(buffer.length === 2 && buffer[0] === '')) {
+        buffer.pop();
+    }
 
-    return buffer.join();
+    return buffer.join('.');
 }
 
 module.exports = resolveFileNameWithoutEnding;
---
+++ src/util/test/resolveFileNameWithoutEnding.spec.js
@@ -0,0 +1,43 @@
+/**
+ * Created on 23.05.18.
+ *
+ * @author Maximilian Beck <maximilian.beck@wtl.de>
+ */
+
+const { describe, it } = require('mocha');
+const { expect } = require('chai');
+const resolveFileNameWithoutEnding = require('../resolveFileNameWithoutEnding');
+
+describe('paths', () => {
+    it('Should be a function', () => {
+        expect(resolveFileNameWithoutEnding).to.be.an.instanceOf(Function);
+    });
+
+    it('Should resolve the file name without ending', () => {
+        expect(resolveFileNameWithoutEnding('file.end')).to.equal('file');
+        expect(resolveFileNameWithoutEnding('file')).to.equal('file');
+        expect(resolveFileNameWithoutEnding('.env')).to.equal('.env');
+        expect(resolveFileNameWithoutEnding('.env.private')).to.equal('.env');
+        expect(resolveFileNameWithoutEnding('test.spec.js')).to.equal('test.spec');
+    });
+
+    it('Should return null when no valid file name is specified', () => {
+        expect(resolveFileNameWithoutEnding('/path/to/file/')).to.be.null;
+        expect(resolveFileNameWithoutEnding('/')).to.be.null;
+        expect(resolveFileNameWithoutEnding('')).to.be.null;
+    });
+
+    it('Should throw when no string is passed', () => {
+        /**
+         * Returns a function that calls resolveFileNameFromPath with the param
+         * @param {*} param : * : The param
+         * @returns {function(): String} : The function
+         */
+        const passAParameter = param => () => resolveFileNameWithoutEnding(param);
+
+        expect(passAParameter(null)).to.throw();
+        expect(passAParameter(1)).to.throw();
+        expect(passAParameter(true)).to.throw();
+        expect(passAParameter('foo')).not.to.throw();
+    });
+});
