97b076c6398630fee2d0a4c281f292c4fe36100c
bmaximilian <maximilianbmail@gmail.com>
2018-05-28 15:37:44 +0200

    0.4.3: add list:directories

---
+++ commandHelper/getUserConfig.js
@@ -0,0 +1,25 @@
+/**
+ * Created on 28.05.18.
+ *
+ * @author Maximilian Beck <maximilian.beck@wtl.de>
+ */
+
+const fs = require('fs');
+const { userConfig: userConfigPath } = require('../src/util/paths');
+
+/**
+ * Returns the user config
+ * @returns {Object|null} : The user config
+ */
+function getUserConfig() {
+    let userConfig = {};
+
+    if (fs.existsSync(userConfigPath)) {
+        // eslint-disable-next-line import/no-dynamic-require, global-require
+        userConfig = require(userConfigPath);
+    }
+
+    return userConfig;
+}
+
+module.exports = getUserConfig;
---
+++ commands/list:directories.js
@@ -0,0 +1,41 @@
+
+const { get, isArray } = require('lodash');
+const chalk = require('chalk');
+const { userConfig: userConfigPath } = require('../src/util/paths');
+const getUserConfig = require('../commandHelper/getUserConfig');
+
+/**
+ * list:directories
+ *
+ * @param {Object} options : Object : The parsed options from process.argv
+ * @param {Object} config : Object : The configuration from ~/.nodexec/config.json
+ * @param {Object} command : Object : The exported command from this directory
+ * @returns {void}
+ */
+function listDirectories(options, config, command) {
+    const userConfig = getUserConfig();
+    console.log();
+
+    if (!userConfig) {
+        console.warn(`No user config exists at ${userConfigPath}`);
+    }
+
+    const directories = get(userConfig, 'directories', []);
+
+    if (!isArray(directories)) {
+        throw new Error(`Directories must be an array in config ${userConfig}`);
+    } else if (directories.length > 0) {
+        console.log(chalk.magenta('Directories:'));
+
+        directories.forEach((directory) => {
+            console.log(`\t${chalk.white(directory)}`);
+        });
+        console.log();
+    }
+}
+
+module.exports = {
+    command: listDirectories,
+    description: 'Lists the configured directories in the user config',
+    scope: '/',
+};
